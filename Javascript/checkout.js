8a44655c76a0450a7d5d897250c61bf966cb38c8
Dan Cartwright <f.d.cartwright@hotmail.com>
2022-09-23 10:00:02 -0700

    completed lesson9-assignment

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -1,21 +1,40 @@
+// JQUERY
 
-const form = document.getElementById('checkout');
-
-form.addEventListener("submit", function(e) {
+const form = $('#checkout').on('submit', function(e) {
     e.preventDefault();
     alert('Thank you. The form information has been received');
-    let submitResponse = {
-        name: form.querySelector('input[name="name"]').value,
-        email: form.querySelector('input[name="name"]').value,
-        street: form.querySelector('input[name="street"]').value,
-        city: form.querySelector('input[name="city"]').value,
-        state: form.querySelector('select[name="state"]').value,
-        zip: form.querySelector('input[name="zip"]').value,
-        firstTimeAdobpter: form.querySelector('input[name="adopter"]').value,
-        pickupLocation:form.querySelector('select[name="location"]').value
+    const submitResponse = {
+        name: this.querySelector('input[name="name"]').value,
+        email: this.querySelector('input[name="name"]').value,
+        street: this.querySelector('input[name="street"]').value,
+        city: this.querySelector('input[name="city"]').value,
+        state: this.querySelector('select[name="state"]').value,
+        zip: this.querySelector('input[name="zip"]').value,
+        firstTimeAdobpter: this.querySelector('input[name="adopter"]').value,
+        pickupLocation: this.querySelector('select[name="location"]').value
     }
     console.log(submitResponse);
 });
+
+// Vanilla JS
+
+// const form = document.getElementById('checkout');
+
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     alert('Thank you. The form information has been received');
+//     const submitResponse = {
+//         name: form.querySelector('input[name="name"]').value,
+//         email: form.querySelector('input[name="name"]').value,
+//         street: form.querySelector('input[name="street"]').value,
+//         city: form.querySelector('input[name="city"]').value,
+//         state: form.querySelector('select[name="state"]').value,
+//         zip: form.querySelector('input[name="zip"]').value,
+//         firstTimeAdobpter: form.querySelector('input[name="adopter"]').value,
+//         pickupLocation:form.querySelector('select[name="location"]').value
+//     }
+//     console.log(submitResponse);
+// });
 
 
 
--- Javascript/index.js
+++ Javascript/index.js
@@ -2,9 +2,30 @@ let dogsTotal = 0.00
 
 function runningTotal(amount) {
     dogsTotal += amount;
-    alert(dogsTotal);
+    $('span.cart-total:eq(0)').html(dogsTotal);
 }
 
 function dogDetails(name, breed, cost) {
     alert('This is ' + name + ', ' + 'it is a ' + breed + ', ' + 'and costs ' + cost + ' to adopt.');
 }
+
+// home page js
+
+$(".dog-house").find("img").hover(
+    function() {
+      $(this).addClass("hover");
+    }, function() {
+      $(this).removeClass("hover");
+    }
+  );
+
+  // dog page js
+
+  $( ".dog-house2" ).find("img").hover(
+    function() {
+      $(this).addClass("hover");
+    }, function() {
+      $(this).removeClass("hover");
+    }
+  );
+
